73da394b1005f5c3d2ba25af4bd3144e7f8a8b25
thesuhas <suhastvs@gmail.com>
2021-05-04 16:44:45 +0530

    added some linux commands

--- script.sh
+++ script.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 # Browser Section
 strace -o Output/browser.txt -c firefox www.google.com www.youtube.com www.github.com en.wikipedia.org
+# Linux Commands
+strace -o Output/Linux/ls.txt -c ls
+strace -o Output/Linux/mkdir.txt -c mkdir test
+strace -o Output/Linux/rmdir.txt -c rmdir test
+strace -o Output/Linux/cat.txt -c cat Output/browser.txt
