6547c500ab9b06ae354f09591b2dd49265c69a24
thesuhas <suhastvs@gmail.com>
2021-05-04 17:21:12 +0530

    echo was added

--- script.sh
+++ script.sh
@@ -6,3 +6,4 @@ strace -o Output/Linux/ls.txt -c ls
 strace -o Output/Linux/mkdir.txt -c mkdir test
 strace -o Output/Linux/rmdir.txt -c rmdir test
 strace -o Output/Linux/cat.txt -c cat Output/browser.txt
+strace -o Output/Linux/echo.txt -c echo "Hello World"
