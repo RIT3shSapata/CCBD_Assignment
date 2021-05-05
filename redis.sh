77a3627182b142118e772acf76d5e3535f3f9407
Suhas Thalanki <54014218+thesuhas@users.noreply.github.com>
2021-05-05 17:06:10 +0530

    Delete redis.sh

--- redis.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-strace -o download.txt -c wget https://download.redis.io/releases/redis-6.2.3.tar.gz
-strace -o extract.txt -c tar xzf redis-6.2.3.tar.gz
-cd redis-6.2.3
-strace -o compile.txt -c make
