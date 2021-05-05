afcc61f8381cbe1d40374bdd30fc04328acd3160
thesuhas <suhastvs@gmail.com>
2021-05-05 17:05:24 +0530

    added basic redis benchmark

--- script.sh
+++ script.sh
@@ -7,3 +7,5 @@ strace -o Output/Linux/mkdir.txt -c mkdir test
 strace -o Output/Linux/rmdir.txt -c rmdir test
 strace -o Output/Linux/cat.txt -c cat Output/browser.txt
 strace -o Output/Linux/echo.txt -c echo "Hello World"
+# Redis Section
+strace -o Output/redis1.txt -c redis-benchmark -h 127.0.0.1 -p 6379 -t set,lpush -n 100000 -q
