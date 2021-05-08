83235c9067823f3f23b5077d374fd48bb7f03c65
Ritesh Sapata <riteshsapta@gmail.com>
2021-05-08 16:18:04 +0530

    Office added

--- script.sh
+++ script.sh
@@ -9,3 +9,5 @@ strace -o Output/Linux/cat.txt -c cat Output/browser.txt
 strace -o Output/Linux/echo.txt -c echo "Hello World"
 # Redis Section
 strace -o Output/redis1.txt -c redis-benchmark -h 127.0.0.1 -p 6379 -t set,lpush -n 100000 -q
+#libreoffice
+strace -o Output/office.txt -c  soffice "macro:///Standard.Module1.marks" test.ods
